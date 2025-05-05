02dbfe566bf3a2640f5a9a6fde3140a749896a97
Yermakhan <yermakhan.serikbayev@nu.edu.kz>
2025-05-05 20:57:18 +0500

    update: rout

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/ainur-aldiyar-toiga-shaqyru", response_class=HTMLResponse)
 async def read_invite(request: Request):
     return templates.TemplateResponse("invite.html", {"request": request})
